d33fb5d447d8279f9080aad7baed5505b5cfdfaf
gkirschnxr <kirschnxr@gmail.com>
2025-10-28 12:03:39 -0300

    add CardMidia, refatorado CarrosselMidias

--- src/app/components/shared/carrossel-midias/carrossel-midias.ts
+++ src/app/components/shared/carrossel-midias/carrossel-midias.ts
@@ -1,15 +1,24 @@
 import { Component, Input } from '@angular/core';
 import { Midia } from '../../../models/midia-api-response';
-import { RouterLink } from '@angular/router';
+
 import { TipoMidia } from '../../../models/tipo-midia';
-import { IconeAvaliacao } from '../icone-avaliacao/icone-avaliacao';
+
+import { CardMidia } from '../card-midia/card-midia';
 
 @Component({
   selector: 'app-carrossel-midias',
-  imports: [RouterLink, IconeAvaliacao],
-  templateUrl: './carrossel-midias.html',
+  imports: [CardMidia],
+  template: `
+    <div class="row flex-nowrap overflow-x-scroll g-3 mt-1 px-1 app-scrollbar-customizado">
+      @for (midia of midias; track midia.id) {
+        <div class="col-7 col-lg-3 col-xl-2">
+          <app-card-midia [tipoMidia]="tipoMidia" [midia]="midia"></app-card-midia>
+        </div>
+      }
+    </div>
+  `,
 })
 export class CarrosselMidias {
   @Input({ required: true }) public tipoMidia: TipoMidia = TipoMidia.Filme;
-  @Input({ required: true }) public midias$: Midia[] = [];
+  @Input({ required: true }) public midias: Midia[] = [];
 }
